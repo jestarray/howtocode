f6cb5e8699b0fe3538686321babaac8c2d7c0238
jestarray <34615798+jestarray@users.noreply.github.com>
2025-07-17 19:57:31 -0700

    11.01 shipping-cost problem typo

--- starter/11.01-shipping-cost_starter.rkt
+++ starter/11.01-shipping-cost_starter.rkt
@@ -23,7 +23,7 @@ Turn all ❌ into ✅ for each step you complete
 |#
 
 #|PROBLEM A:
-Notice the Data Definitions for Standard, Express, and International
+Notice the Data Definitions for Standard, Express, and International are done.
 Finish the uncompleted steps ❌ of Data Design above for "ShippingMethod"(examples and template)
 |#
 

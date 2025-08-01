1aa2be16975bea3b5a92472ab88ad28f32f1be59
jestarray <34615798+jestarray@users.noreply.github.com>
2025-08-01 09:09:47 -0700

    slide_mode() now takes color arguments

--- slide_mode.js
+++ slide_mode.js
@@ -1,8 +1,13 @@
-function slide_mode() {
+/// call with slide_mode("black", "white"), for black bg and white text, although this may conflict with table coloring. In the future, it is better if we have a seperate toggle for dark mode independent of doing it here. All this function should do is grab all slide mode elements
+function slide_mode(bg_color, color) {
   //if there isnt enough content for a slide and it looks to small, just zoom in! it also helps emphasizes the point
   let body = document.getElementsByTagName("body")[0];
-  body.style.background = "black";
-  body.style.color = "white";
+  if (bg_color != undefined) {
+    body.style.background = bg_color;
+  }
+  if (color != undefined) {
+    body.style.color = color;
+  }
   let all = document.getElementsByTagName("article")[0].children;
   Array.from(all).forEach((v) => {
     if (v.className !== "slide") {

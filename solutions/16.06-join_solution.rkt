967529213f0a3db1a7167da9991585ffdfd09a35
jestarray <34615798+jestarray@users.noreply.github.com>
2025-08-28 19:24:44 -0700

    16.06 join: fix problem statement typo

--- solutions/16.06-join_solution.rkt
+++ solutions/16.06-join_solution.rkt
@@ -36,7 +36,7 @@ Turn all ❌ into ✅ for each step you complete
 
 #|PROBLEM A:
 Notice how the data design step is done. 
-Write a function join that consumes NEListOfString, and additional delimiter string(delimiter).
+Write a function join that consumes NEListOfString, and a additional delimiter string(delimiter).
 The function produces a single string consisting of all the list elements joined together,
 separated by copies of delimiter.
 For example:
